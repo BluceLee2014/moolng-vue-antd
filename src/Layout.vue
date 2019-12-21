7b0987c0482b607fa7364413d4c53c12d830e8cf
BluceLee <543232022@qq.com>
2019-12-21 16:46:57 +0800

    动态tads的添加

--- src/Layout.vue
+++ src/Layout.vue
@@ -4,12 +4,15 @@
             <div class="logo"/>
         </a-layout-header>
         <a-layout>
+            <!--            <MoolngMenu></MoolngMenu>-->
             <a-layout-sider width="200" style="background: #fff">
                 <a-menu mode="inline" :defaultSelectedKeys="['1']" :defaultOpenKeys="['sub1']"
                         :style="{ height: '100%', borderRight: 0 }">
                     <a-sub-menu v-for="m in menuData" :key="m.id" :title="m.title">
                         <span slot="title"><a-icon :type="m.icon"/>{{m.title}}</span>
-                        <a-menu-item v-for="subNote in m.subList" :key="subNote.uri" :onchange="addTag()">
+                        <a-menu-item v-for="subNote in m.subList"
+                                     :key="'{path:\''+subNote.uri + '\',name:\''+subNote.routerName + '\'}'"
+                                     v-on:click="addTag" :title="subNote.title">
                             {{subNote.title}} - {{subNote.uri}}
                             <router-link :to="subNote.uri"></router-link>
                             <!--                            <a href="https://www.baidu.com" target="_blank"></a>-->
@@ -25,7 +28,7 @@
                         {{a}}
                         <!--                        <input ref="moolngInput" v-on:click="mtest"/>-->
                         <router-link to="/index">跳转到指定Tab</router-link>
-<!--                        <button v-on:click="getMenuInfo">点击</button>-->
+                        <!--                        <button v-on:click="getMenuInfo">点击</button>-->
                         <!--                        <router-view :name="pane.routerName"></router-view>-->
                     </a-tab-pane>
                 </a-tabs>
@@ -35,193 +38,215 @@
 </template>
 
 <script>
-export default {
-  name: 'Layout',
-  data () {
-    const tabsData = [
-      {title: '首页', content: 'Content of Tab 1', key: '-1', closable: false}
-      // {title: 'Tab 2', content: 'Content of Tab 2', key: '-2'},
-    ]
-    const showTabsData = {}
-    const menuInfo = {}
-    const existTabs = []
-    return {
-      collapsed: false,
-      menuData: [
-        {
-          id: 1,
-          icon: 'user',
-          title: 'subnav 1',
-          subList: [
-            {
-              id: 2,
-              icon: 'user',
-              title: 'option1',
-              uri: '/r1'
-            },
-            {
-              id: 3,
-              icon: 'user',
-              title: 'option2',
-              uri: '/r2'
-            },
-            {
-              id: 4,
-              icon: 'user',
-              title: 'option3',
-              uri: '/r3'
+    // import MoolngMenu from "./components/MoolngMenu";
+
+    export default {
+        name: 'Layout',
+        data() {
+            const tabsData = [
+                {title: '首页', content: 'Content of Tab 1', key: '-1', closable: false}
+                // {title: 'Tab 2', content: 'Content of Tab 2', key: '-2'},
+            ]
+            const showTabsData = {}
+            const menuInfo = {}
+            const existTabs = []
+            return {
+                collapsed: false,
+                menuData: [
+                    {
+                        id: 1,
+                        icon: 'user',
+                        title: 'subnav 1',
+                        subList: [
+                            {
+                                id: 2,
+                                icon: 'user',
+                                title: 'option1',
+                                uri: '/r1',
+                                routerName: 'R1'
+                            },
+                            {
+                                id: 3,
+                                icon: 'user',
+                                title: 'option2',
+                                uri: '/r2',
+                                routerName: 'R2'
+                            },
+                            {
+                                id: 4,
+                                icon: 'user',
+                                title: 'option3',
+                                uri: '/r3',
+                                routerName: 'R3'
+                            }
+                        ]
+                    },
+                    {
+                        id: 5,
+                        icon: 'laptop',
+                        title: 'subnav 2',
+                        subList: []
+                    },
+                    {
+                        id: 6,
+                        icon: 'notification',
+                        title: 'subnav 3',
+                        subList: []
+                    }
+                ],
+                activeKey: tabsData[0].key,
+                tabsData,
+                showTabsData,
+                menuInfo,
+                existTabs,
+                a: 0,
+                newTabIndex: 3
             }
-          ]
         },
-        {
-          id: 5,
-          icon: 'laptop',
-          title: 'subnav 2',
-          subList: []
+        methods: {
+            mtest() {
+                console.log(this)
+            },
+            getMenuInfo() {
+                /**
+                 const route = this.$route
+                 let routerName = route.name
+                 let uri = route.path
+                 console.log(route)
+                 // console.log(this)
+                 console.log(e)
+                 // console.log("------getMenuInfo-------")
+                 // console.log(this.$refs)
+                 const obj = e.item.$slots
+                 // console.log(obj)
+                 const a = obj['default']
+                 // console.log(a[0])
+                 // console.log(a[0].text)
+                 let id = e.key
+                 let name = a[0].text
+                 // id: Number, name: '', uri: '',routerName: ''
+                 this.showTabsData[id] = {id: id, name: name, uri: uri, routerName: routerName}
+                 // this.menuInfo  = {id: id, name: name, uri: uri, routerName: routerName}
+                 **/
+                this.addTag()
+            },
+            callback(key) {
+                console.log(key)
+            },
+            onEdit(targetKey, action) {
+                this[action](targetKey)
+            },
+            getShowTabs(uri) {
+                // const route = this.$route
+                // let routerName = route.name
+                // let uri = route.path
+                // console.log(this.existTabs)
+                if (this.existTabs.indexOf(uri) == -1) {
+                    this.existTabs.push(uri)
+                    return false
+                }
+                return true
+
+            },
+            addTag(e) {
+                let val = e.key
+                let route = eval("(" + val + ")")
+                let item = e.item
+                // const route = this.$route
+                console.log(item.title)
+                console.log(route)
+                let routerName = route.name
+                let name = item.title
+                let uri = route.path
+
+                if (this.getShowTabs(uri)) {
+                    this.activeKey = uri
+                    return
+                }
+                const panes = this.tabsData
+                const activeKey = uri
+                panes.push({
+                    title: name,
+                    content: `Content of new Tab ${activeKey}`,
+                    key: activeKey,
+                    routerName: routerName
+                })
+                this.tabsData = panes
+                this.activeKey = activeKey
+            },
+            add() {
+                console.log('add')
+                const panes = this.tabsData
+                const activeKey = `newTab${this.newTabIndex++}`
+                panes.push({
+                    title: `New Tab ${activeKey}`,
+                    content: `Content of new Tab ${activeKey}`,
+                    key: activeKey
+                })
+                this.tabsData = panes
+                this.activeKey = activeKey
+            },
+            remove(targetKey) {
+                console.log('remove', targetKey, this.existTabs)
+                let activeKey = this.activeKey
+                let lastIndex
+                this.tabsData.forEach((pane, i) => {
+                    if (pane.key === targetKey) {
+                        lastIndex = i - 1
+                        this.arryRemove(this.existTabs, targetKey)
+                    }
+                })
+                const panes = this.tabsData.filter(pane => pane.key !== targetKey)
+                if (panes.length && activeKey === targetKey) {
+                    if (lastIndex >= 0) {
+                        activeKey = panes[lastIndex].key
+                    } else {
+                        activeKey = panes[0].key
+                    }
+                }
+                this.tabsData = panes
+                this.activeKey = activeKey
+            },
+            getUrl() {
+                const route = this.$route
+                console.log(route)
+            },
+            two() {
+                console.log(this.$route)
+            },
+            arryRemove(arr, removeVal) {
+                let index = arr.indexOf(removeVal);
+                console.log(index)
+                if (index > -1) {
+                    arr.splice(index, 1);
+                }
+            }
         },
-        {
-          id: 6,
-          icon: 'notification',
-          title: 'subnav 3',
-          subList: []
+        // computed () {
+        // },
+        mounted() {
+            // console.log(this)
+            // this.a++
+        },
+        watch: {
+            // $route: {
+            // handler(){
+            //   this.addTag()
+            // }
+
+            // handler:function(val, oldVal){
+            //   this.$nextTick(function(){  //页面加载完成后执行
+            //     this.resetCopyright()
+            //   })
+            // },
+            // // 深度观察监听
+            // deep: true
+            // }
+        },
+        components: {
+            // MoolngMenu
         }
-      ],
-      activeKey: tabsData[0].key,
-      tabsData,
-      showTabsData,
-      menuInfo,
-      existTabs,
-      a: 0,
-      newTabIndex: 3
     }
-  },
-  methods: {
-    mtest () {
-      console.log(this)
-    },
-    getMenuInfo () {
-      /**
-       const route = this.$route
-       let routerName = route.name
-       let uri = route.path
-       console.log(route)
-       // console.log(this)
-       console.log(e)
-       // console.log("------getMenuInfo-------")
-       // console.log(this.$refs)
-       const obj = e.item.$slots
-       // console.log(obj)
-       const a = obj['default']
-       // console.log(a[0])
-       // console.log(a[0].text)
-       let id = e.key
-       let name = a[0].text
-       // id: Number, name: '', uri: '',routerName: ''
-       this.showTabsData[id] = {id: id, name: name, uri: uri, routerName: routerName}
-       // this.menuInfo  = {id: id, name: name, uri: uri, routerName: routerName}
-       **/
-      this.addTag()
-    },
-    callback (key) {
-      console.log(key)
-    },
-    onEdit (targetKey, action) {
-      this[action](targetKey)
-    },
-    getShowTabs () {
-      const route = this.$route
-      // let routerName = route.name
-      let uri = route.path
-      console.log(this.existTabs)
-      if (this.existTabs.indexOf(uri) == -1) {
-        this.existTabs.push(uri)
-        return false
-      }
-      return true
-
-    },
-    addTag () {
-      const route = this.$route
-      console.log(route)
-      let routerName = route.name
-      let name = route.meta.title
-      let uri = route.path
-
-      if (this.getShowTabs()) {
-        this.activeKey = uri
-        return
-      }
-      const panes = this.tabsData
-      const activeKey = uri
-      panes.push({
-        title: name,
-        content: `Content of new Tab ${activeKey}`,
-        key: activeKey,
-        routerName: routerName
-      })
-      this.tabsData = panes
-      this.activeKey = activeKey
-    },
-    add () {
-      const panes = this.tabsData
-      const activeKey = `newTab${this.newTabIndex++}`
-      panes.push({
-        title: `New Tab ${activeKey}`,
-        content: `Content of new Tab ${activeKey}`,
-        key: activeKey
-      })
-      this.tabsData = panes
-      this.activeKey = activeKey
-    },
-    remove (targetKey) {
-      let activeKey = this.activeKey
-      let lastIndex
-      this.panes.forEach((pane, i) => {
-        if (pane.key === targetKey) {
-          lastIndex = i - 1
-        }
-      })
-      const panes = this.panes.filter(pane => pane.key !== targetKey)
-      if (panes.length && activeKey === targetKey) {
-        if (lastIndex >= 0) {
-          activeKey = panes[lastIndex].key
-        } else {
-          activeKey = panes[0].key
-        }
-      }
-      this.panes = panes
-      this.activeKey = activeKey
-    },
-    getUrl () {
-      const route = this.$route
-      console.log(route)
-    },
-    two () {
-      console.log('two')
-    }
-  },
-  // computed () {
-  // },
-  mounted () {
-    console.log(this)
-    this.a++
-  },
-  watch: {
-    // $route: {
-    // handler(){
-    //   this.addTag()
-    // }
-
-    // handler:function(val, oldVal){
-    //   this.$nextTick(function(){  //页面加载完成后执行
-    //     this.resetCopyright()
-    //   })
-    // },
-    // // 深度观察监听
-    // deep: true
-    // }
-  }
-}
 </script>
 
 <style scoped>
---
+++ src/components/MoolngMenu.vue
@@ -0,0 +1,100 @@
+<template>
+    <a-layout-sider width="200" style="background: #fff">
+        <a-menu mode="inline" :defaultSelectedKeys="['1']" :defaultOpenKeys="['sub1']"
+                :style="{ height: '100%', borderRight: 0 }">
+            <a-sub-menu v-for="m in menuData" :key="m.id" :title="m.title">
+                <span slot="title"><a-icon :type="m.icon"/>{{m.title}}</span>
+                <a-menu-item v-for="subNote in m.subList"
+                             :key="'{uri:\''+subNote.uri + '\',routerName:\''+subNote.routerName + '\'}'"
+                             v-on:click="two">
+                    {{subNote.title}} - {{subNote.uri}}
+                    <router-link :to="subNote.uri"></router-link>
+                    <!--                            <a href="https://www.baidu.com" target="_blank"></a>-->
+                </a-menu-item>
+            </a-sub-menu>
+        </a-menu>
+    </a-layout-sider>
+</template>
+
+<script>
+    export default {
+        name: "MoolngMenu",
+        data() {
+            return {
+                menuData: [
+                    {
+                        id: 1,
+                        icon: 'user',
+                        title: 'subnav 1',
+                        subList: [
+                            {
+                                id: 2,
+                                icon: 'user',
+                                title: 'option1',
+                                uri: '/r1',
+                                routerName: 'R1'
+                            },
+                            {
+                                id: 3,
+                                icon: 'user',
+                                title: 'option2',
+                                uri: '/r2'
+                            },
+                            {
+                                id: 4,
+                                icon: 'user',
+                                title: 'option3',
+                                uri: '/r3'
+                            }
+                        ]
+                    },
+                    {
+                        id: 5,
+                        icon: 'laptop',
+                        title: 'subnav 2',
+                        subList: []
+                    },
+                    {
+                        id: 6,
+                        icon: 'notification',
+                        title: 'subnav 3',
+                        subList: []
+                    }
+                ]
+            }
+        },
+        methods: {
+            two(e) {
+                let val = e.key
+                console.log(val)
+                console.log(eval("(" + val + ")"))
+                // console.log(this.$route)
+            },
+            onOpenChange(openKeys) {
+                console.log(openKeys)
+                // const latestOpenKey = openKeys.find(key => this.openKeys.indexOf(key) === -1);
+                // if (this.rootSubmenuKeys.indexOf(latestOpenKey) === -1) {
+                //     this.openKeys = openKeys;
+                // } else {
+                //     this.openKeys = latestOpenKey ? [latestOpenKey] : [];
+                // }
+            },
+            titleClick(e) {
+                console.log('titleClick', e);
+            },
+            handleClick(e) {
+                console.log('click', e);
+                console.log(this.$route)
+            },
+        },
+        watch: {
+            openKeys(val) {
+                console.log('openKeys', val);
+            },
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
